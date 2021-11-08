5204ccfcead091a659d38b1859715d9bc8c403d9
carlossilva162 <carlos.silva16@estudante.ufla.br>
2021-11-08 20:31:57 -0300

    create divisao arquiv

--- trabalho.cpp
+++ trabalho.cpp
@@ -2,25 +2,27 @@
 #include <cstring>
 #include <fstream>
 
-
 using namespace std;
 
-
 // dados armazenados no arquivo
-struct pacote {
+struct pacote
+{
     unsigned indice;
     float tempo;
     char origem[40];
     char destino[40];
     char protocolo[18];
     unsigned tamanho;
-    char informacao[100]; 
+    char informacao[100];
 };
 
-
-void le_arquivo_bin(ifstream& arquivo) {
+void le_arquivo_bin(ifstream &arquivo)
+{
     pacote umPacote;
-    while(arquivo.read((char*) &umPacote, sizeof(umPacote))) {
+    int i = 0;
+    while (arquivo.read((char *)&umPacote, sizeof(umPacote)))
+    {
+        i++;
         cout << umPacote.indice << "; ";
         cout << umPacote.tempo << "; ";
         cout << umPacote.origem << "; ";
@@ -29,13 +31,49 @@ void le_arquivo_bin(ifstream& arquivo) {
         cout << umPacote.tamanho << "; ";
         cout << umPacote.informacao << endl;
     }
+    cout << "Total de pacotes lidos: " << i << endl;
+}
+
+void criarArquivoSeparado(ifstream &arquivo)
+{
+    pacote umPacote;
+    ofstream MyFile("teste1.txt");
+    ofstream MyFile2("teste2.txt");
+    int count = 0;
+    int contadorDeBlocos = 0;
+
+    cout << sizeof(umPacote) << endl;
+    while (arquivo.read((char *)&umPacote, sizeof(umPacote)))
+    {
+        contadorDeBlocos++;
+        if(contadorDeBlocos == 4) {
+            contadorDeBlocos=0;
+            count++;
+        }
+        if(count%2 == 0) {
+            MyFile << umPacote.indice << umPacote.tempo << umPacote.origem << umPacote.destino << umPacote.protocolo << umPacote.tamanho << umPacote.informacao << endl;
+        } else {
+            MyFile2 << umPacote.indice << umPacote.tempo << umPacote.origem << umPacote.destino << umPacote.protocolo << umPacote.tamanho << umPacote.informacao << endl;
+        }
+
+    }
+
+    MyFile.close();
+    MyFile2.close();
 }
 
 
-int main(){
-    ifstream arquivo_bin_read("captura_pacotes.bin",ios::binary);
+void mergeSortInterativo(){
 
-    le_arquivo_bin(arquivo_bin_read);
+    
+}
 
+int main()
+{
+    ifstream arquivo_bin_read("captura_pacotes.bin", ios::binary);
+    ifstream arquivo_Txt("filename.txt", ios::binary);
+
+    le_arquivo_bin(arquivo_Txt);
+    criarArquivoSeparado(arquivo_bin_read);
     return 0;
 }
