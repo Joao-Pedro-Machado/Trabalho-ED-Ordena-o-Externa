ed5abced5ac2d34f24d77d096106f762e4b59de9
Joao Pedro <joao.machado0601@gmail.com>
2021-11-12 19:22:23 -0300

    começando a separar os dados

--- trabalho.cpp
+++ trabalho.cpp
@@ -4,6 +4,20 @@
 
 using namespace std;
 
+/*
+    Lógica do mergeSort:
+        [x] Separar o arquivo original em f1 e f2  ordenando os pacotes
+        [] Gerar dois arquivos de saída
+        [] Iniciar a intercalação
+            [] s1 e s2 tem que possuir blocos de tamanho dobrado em relação as entradas
+            [] Os arquivos vazios viram saidas e os cheios viram entradas
+            [] repetir até formar 1 bloco >= 100.000 ou todos vazios exceto 1
+
+    Obs: O numero de pacotes só pode ser diferente em 1, em relação aos outros
+*/
+
+
+
 // dados armazenados no arquivo
 struct pacote
 {
@@ -78,7 +92,6 @@ void merge(pacote *vetor, int inicio, int meio, int fim)
 void mergeSort(pacote *vetor, int inicio, int fim)
 {
 
-    cout << " inicio " << inicio << " fim " << fim << endl;
     if (inicio < fim)
     {
         int meio = (inicio + fim) / 2;
@@ -91,13 +104,13 @@ void mergeSort(pacote *vetor, int inicio, int fim)
 void lerPrimeiroArquivo(ifstream &arquivo)
 {
     pacote umPacote;
-    pacote batatinha[100];
+    pacote vetor[1000];
     int i = 0;
     while (arquivo.read((char *)&umPacote, sizeof(umPacote)))
     {
         i++;
 
-        batatinha[i] = umPacote;
+        vetor[i] = umPacote;
         // if (i = 10)
         // {
         //     // for (int j = 0; j < 10; j++)
@@ -113,41 +126,61 @@ void lerPrimeiroArquivo(ifstream &arquivo)
         // }
         if (i == 10)
         {
-            mergeSort(batatinha, 0, i);
+            mergeSort(vetor, 0, i);
 
-            for (int r = 0; r < 10; r++)
+            for (int r = 0; r < 1000; r++)
             {
-                cout << "strinhg " << batatinha[r].indice << endl;
+                cout << "strinhg " << vetor[r].indice << endl;
             }
         }
     };
 }
 
+void salvaVetor(pacote *vetor, ofstream &file, int tamanho){
+    for(int i = 0; i < tamanho; i++) {
+        file.write((const char *) &vetor[i], sizeof(pacote));
+    }
+}
+
 void criarArquivoSeparado(ifstream &arquivo)
 {
     pacote umPacote;
-    ofstream MyFile("teste1.txt");
-    ofstream MyFile2("teste2.txt");
+    ofstream MyFile("f1.bin", ios::binary);
+    ofstream MyFile2("f2.bin", ios::binary);
     int count = 0;
-    int contadorDeBlocos = 0;
+    int contadorDeBlocos = 1;
+    int tamanho = 1000;
+    pacote vetor[tamanho];
+    int i = 0;
 
     while (arquivo.read((char *)&umPacote, sizeof(umPacote)))
     {
-        contadorDeBlocos++;
-        if (contadorDeBlocos == 4)
+        if (contadorDeBlocos == 1000)
         {
             contadorDeBlocos = 0;
+            mergeSort(vetor, 0, tamanho);
+            if (count % 2 == 0){
+                salvaVetor(vetor, MyFile, tamanho);
+            } else {
+                salvaVetor(vetor, MyFile2, tamanho);
+            }
             count++;
+        } else {
+            vetor[i] = umPacote;
+            i++;
+            contadorDeBlocos++;
         }
-        if (count % 2 == 0)
+    }
+        /*if (count % 2 == 0)
         {
+            MyFile.write((const char *) &vetor, sizeof(pacote) * tamanho);
+
             MyFile << umPacote.indice << umPacote.tempo << umPacote.origem << umPacote.destino << umPacote.protocolo << umPacote.tamanho << umPacote.informacao << endl;
         }
         else
         {
             MyFile2 << umPacote.indice << umPacote.tempo << umPacote.origem << umPacote.destino << umPacote.protocolo << umPacote.tamanho << umPacote.informacao << endl;
-        }
-    }
+        }*/
 
     MyFile.close();
     MyFile2.close();
@@ -161,6 +194,6 @@ int main()
 
     // le_arquivo_bin(arquivo_Txt);
     criarArquivoSeparado(arquivo_bin_read);
-    lerPrimeiroArquivo(arquivo_Txt1);
+    //lerPrimeiroArquivo(arquivo_Txt1);
     return 0;
 }
